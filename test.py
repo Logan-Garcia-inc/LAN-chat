732e11a6c4e0ef52bf0cb3e84119445fcbd2fbdf
25garcial <91680716+25garcial@users.noreply.github.com>
2024-09-03 13:33:58 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import threading
-def client():
-    os.system("python client.py")
-def server():
-    os.system("python server.py")
-threading.Thread(target=client, args=()).start()
-threading.Thread(target=server,args=()).start()
